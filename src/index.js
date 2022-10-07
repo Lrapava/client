222108325b79a74c3f6e0d2a32a93932df0b9efa
Lrapava <luka.rapava2004@gmail.com>
2022-10-07 23:04:18 +0400

    complying with updates to client rendering API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom"
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-// import { Route, Link, Routes, BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from "./App"
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(<App/>);
